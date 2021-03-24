38689bc716016d1015212a0c4b9d3be2953ede6d
JachymFojtik <jachym.foj@gmail.com>
2021-03-24 23:49:41 +0100

    Add project files.

---
+++ LowChange/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LowChange
+{
+
+    class Program
+    {
+        static Dictionary<int,int> coinCount = new Dictionary<int, int> { {50,0}, { 20, 0 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 1, 0 } };
+        static void LowestChange(int x, int y)
+        {
+            int back = x - y;
+            int high = 50;
+            if (back>=1)
+            {
+                foreach (var item in coinCount)
+                {
+                    if (back >= item.Key)
+                    {
+                        high = item.Key;
+                        coinCount[item.Key] = back / item.Key;
+                        break;
+                    }
+                }
+                LowestChange(x - (back / high)*high, y );
+            }
+            else
+            {
+                foreach (var item in coinCount)
+                {
+                    if (item.Value != 0)
+                    {
+                        Console.WriteLine($"{item.Value} x {item.Key},-Kč");
+                    }
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            LowestChange(600,422);
+            Console.ReadKey();
+        }
+    }
+}
